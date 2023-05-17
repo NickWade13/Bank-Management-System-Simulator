c099b5ceb441aa1e78d98f08ee4d210affbff2b0
Nicholas <nick-wade@live.co.uk>
2023-05-17 15:36:04 +0100

    Framework of code laid out

---
+++ bank_management.py
@@ -0,0 +1,66 @@
+class Account:
+    def __init__(self, account_number, account_holder_name, balance=0.0):
+        # Initialize account attributes
+        pass
+    
+    def deposit(self, amount):
+        # Perform deposit operation
+        pass
+    
+    def withdraw(self, amount):
+        # Perform withdrawal operation
+        pass
+    
+    def get_account_details(self):
+        # Retrieve account details
+        pass
+
+
+class Bank:
+    def __init__(self):
+        # Initialize the bank with empty account list
+        pass
+    
+    def create_account(self, account_number, account_holder_name, initial_balance):
+        # Create a new account
+        pass
+    
+    def perform_transaction(self, account_number, amount, transaction_type):
+        # Perform a transaction (deposit/withdrawal) on an account
+        pass
+    
+    def display_account_details(self, account_number):
+        # Display account details
+        pass
+    
+    def generate_reports(self):
+        # Generate reports
+        pass
+
+
+def load_data_from_file():
+    # Load account data and transaction history from file
+    pass
+
+
+def save_data_to_file():
+    # Save account data and transaction history to file
+    pass
+
+
+def display_menu():
+    # Display menu options
+    pass
+
+
+def handle_user_input(option):
+    # Handle user input and call appropriate methods
+    pass
+
+
+# Main program
+if __name__ == "__main__":
+    # Load data from file
+    # Display menu
+    # Handle user input
+    # Save data to file
